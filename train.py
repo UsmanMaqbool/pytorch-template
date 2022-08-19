416bda3ca3c44d17cc6d0a26c71e3fd63945119e
usmanmaqbool <usmanmaqbool@outlook.com>
2022-08-19 11:06:11 +0800

    updates

--- train.py
+++ train.py
@@ -1,15 +1,48 @@
 import argparse
 from parse_config import ConfigParser
-
 import collections
 
 import torch
 import numpy as np
 
+import data_loader.data_loaders as module_data
+from trainer import Trainer
+
 
 def main(config):
     logger = config.get_logger('train')
+    
+    # setup data_loader instances
+    data_loader = config.init_obj('data_loader', module_data)
+    valid_data_loader = data_loader.split_validation()
 
+    # build model architecture, then print to console
+    model = config.init_obj('arch', module_arch)
+    logger.info(model)
+
+    # prepare for (multi-device) GPU training
+    device, device_ids = prepare_device(config['n_gpu'])
+    model = model.to(device)
+    if len(device_ids) > 1:
+        model = torch.nn.DataParallel(model, device_ids=device_ids)
+
+    # get function handles of loss and metrics
+    criterion = getattr(module_loss, config['loss'])
+    metrics = [getattr(module_metric, met) for met in config['metrics']]
+
+    # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
+    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+
+    trainer = Trainer(model, criterion, metrics, optimizer,
+                      config=config,
+                      device=device,
+                      data_loader=data_loader,
+                      valid_data_loader=valid_data_loader,
+                      lr_scheduler=lr_scheduler)
+
+    trainer.train()
 
 
 if __name__ == '__main__':
@@ -24,6 +57,9 @@ if __name__ == '__main__':
     # custom cli options to modify configuration from default values given in json file.
     CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
     args_custom = [
+        CustomArgs(['--n', '--name'], type=str, target='name'),
+        CustomArgs(['--dpath', '--dataset_path'], type=str, target='data_loader;args;data_dir'),
+        CustomArgs(['--spath', '--save_path'], type=str, target='trainser;save_dir'),
         CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
         CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size')
     ]
